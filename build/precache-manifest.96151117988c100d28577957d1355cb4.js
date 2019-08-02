eb9f54a2cbb822ebfc5c49344708984d6d362303
sudeshsharma <sudesh18sharma@gmail.com>
2019-08-02 12:25:13 +0530

    Blogs

---
+++ build/precache-manifest.96151117988c100d28577957d1355cb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e2311b44dc53939515f922398f872b",
+    "url": "/blogs/index.html"
+  },
+  {
+    "revision": "33d6f3bf5b67f61acc51",
+    "url": "/blogs/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "0b245f6e41e3a0446090",
+    "url": "/blogs/static/js/2.16d9ab28.chunk.js"
+  },
+  {
+    "revision": "33d6f3bf5b67f61acc51",
+    "url": "/blogs/static/js/main.44043223.chunk.js"
+  },
+  {
+    "revision": "07af3d326506b8d65e49",
+    "url": "/blogs/static/js/runtime~main.8841e5c7.js"
+  },
+  {
+    "revision": "c8c07a6c8c28acb46be82cf7d32a5cc2",
+    "url": "/blogs/static/media/GST1.c8c07a6c.jpg"
+  },
+  {
+    "revision": "9136c53cb0a686b7c1fd850578cbfc4e",
+    "url": "/blogs/static/media/Steve.9136c53c.jpg"
+  },
+  {
+    "revision": "85eabf27c69c342fb8ad19f9d0db0fa5",
+    "url": "/blogs/static/media/blog3.85eabf27.jpg"
+  },
+  {
+    "revision": "078b0c87d46a500e085528723566cae2",
+    "url": "/blogs/static/media/regi.078b0c87.png"
+  }
+]);
